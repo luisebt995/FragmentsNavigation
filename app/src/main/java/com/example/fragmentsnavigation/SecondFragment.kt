2d20b33bf388d4d3002606c9dcb0b9178c8140a8
Luis Enrique <luisebt995@gmail.com>
2024-02-16 19:47:28 +0100

    Navigation through three fragments and passing arguments.

--- app/src/main/java/com/example/fragmentsnavigation/SecondFragment.kt
+++ app/src/main/java/com/example/fragmentsnavigation/SecondFragment.kt
@@ -6,6 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 
 
@@ -16,9 +19,18 @@ class SecondFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        val root = inflater.inflate(R.layout.fragment_second, container, false)
+        val btnSecondThird = root.findViewById<Button>(R.id.btnSecondThird)
+        val editProvincia = root.findViewById<EditText>(R.id.editProvincia)
+
+        btnSecondThird.setOnClickListener {
+            val action = SecondFragmentDirections.actionSecondFragmentToThirdFragment(editProvincia.text.toString())
+            findNavController().navigate(action)
+        }
         Log.i("SecondFragment",args.name.toString())
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+
+        return root
     }
 
 }
